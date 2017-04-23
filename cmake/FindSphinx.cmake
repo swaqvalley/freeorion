00e463560176adec8d8c039fc27ae1cabb9a443b
Marcel Metz <mmetz@adrian-broher.net>
2017-04-23 12:31:36 +0200

    Add CMake FindSphinx module for python documentation

---
+++ cmake/FindSphinx.cmake
@@ -0,0 +1,58 @@
+#.rst
+# FindSphinx
+# ----------
+#
+# Find the Sphinx documentation generator.
+#
+# Result Variables
+# ^^^^^^^^^^^^^^^^
+#
+# This defines the following variables:
+#
+# ::
+#
+#   SPHINX_BUILD_EXECUTABLE     - The path to the sphinx-build command.
+#   SPHINX_APIDOC_EXECUTABLE    - The path to the sphinx-apidoc command.
+#   SPHINX_FOUND                - Was Sphinx found or not?
+#   SPHINX_VERSION              - The version reported by sphinx-build --version
+
+
+find_program(SPHINX_BUILD_EXECUTABLE
+    NAMES sphinx-build
+    DOC "Sphinx documentation generator tool"
+)
+
+find_program(SPHINX_APIDOC_EXECUTABLE
+    NAMES sphinx-apidoc
+    DOC "Sphinx API doc generator tool"
+)
+
+if(SPHINX_BUILD_EXECUTABLE)
+    execute_process(
+        COMMAND
+            "${SPHINX_BUILD_EXECUTABLE}" "--version"
+        OUTPUT_VARIABLE
+            SPHINX_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    string(REGEX REPLACE
+        "Sphinx \\(sphinx-build\\) ([0-9]+).([0-9]+).([0-9]+)"
+        "\\1.\\2.\\3"
+        SPHINX_VERSION
+        "${SPHINX_VERSION}"
+    )
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Sphinx
+    REQUIRED_VARS
+        SPHINX_BUILD_EXECUTABLE
+        SPHINX_APIDOC_EXECUTABLE
+    VERSION_VAR
+        SPHINX_VERSION
+)
+
+mark_as_advanced(
+    SPHINX_BUILD_EXECUTABLE
+    SPHINX_APIDOC_EXECUTABLE
+)
