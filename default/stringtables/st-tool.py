be1a477b6e06cec77e8144610218c1b58f3e5731
Marcel Metz <mmetz@adrian-broher.net>
2018-01-11 00:49:35 +0100

    Add web based editor to st-tool

--- default/stringtables/st-tool.py
+++ default/stringtables/st-tool.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 from __future__ import print_function
 import argparse
+import cgi
 import datetime
 import errno
+import json
 import os
 import re
 import subprocess
+import time
+import urlparse
+import urllib2
+import webbrowser
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from collections import namedtuple, OrderedDict
 
 INTERNAL_REFERENCE_PATTERN = r'\[\[((?:(?:encyclopedia|(?:building|field|meter)type|predefinedshipdesign|ship(?:hull|part)|special|species|tech) )?)({})\]\]'
@@ -311,6 +318,255 @@ class StringTable(object):
             right=right, right_only=right_only, right_older=right_older,
             untranslated=untranslated)
 
+    @staticmethod 
+    def sync(reference, source):
+        entries = []
+
+        for item in reference.items():
+            if isinstance(item, StringTableEntry):
+                if item.key in source:
+                    source_item = source[item.key]
+                    entries.append(StringTableEntry(
+                        item.key,
+                        item.keyline,
+                        source_item.value,
+                        item.notes,
+                        source_item.value_times
+                    ))
+                else:
+                    entries.append(StringTableEntry(
+                        item.key,
+                        item.keyline,
+                        None,
+                        item.notes,
+                        item.value_times
+                    ))
+            else:
+                entries.append(item)
+
+        return StringTable(source.fpath, source.language, source.notes, source.includes, entries)
+
+
+class EditServerHandler(BaseHTTPRequestHandler):
+    body = '''
+<html>
+    <head>
+        <title>Editing {source}</title>
+        <style>
+            @import url("/mootools-inlineeditor.css");
+            table {{
+                table-layout: fixed;
+                width: 100%;
+            }}
+            td {{
+                vertical-align: top;
+                white-space: pre-wrap;
+            }}
+            .status-recent td.translation {{
+                background-color: lime;
+                color: black;
+            }}
+            .status-stale td.translation {{
+                background-color: yellow;
+                color: black;
+            }}
+            .status-untranslated td.translation {{
+                background-color: red;
+                color: white;
+            }}
+            .entry td.source {{
+                background-color: lightgrey;
+                color: black;
+            }}
+        </style>
+        <script type="text/javascript" src="/mootools-core.js"></script>
+        <script type="text/javascript" src="/mootools-more.js"></script>
+        <script type="text/javascript" src="/mootools-inlineeditor.js"></script>
+        <script type="text/javascript" src="/mootools-inlineeditor-textarea.js"></script>
+        <script type="text/javascript">
+            function toggle_entries_by_status(status, visible) {{
+                toggle = $$('.toggle-' + status + ' span')[0];
+                visible = toggle.retrieve('visible', false);
+
+                $$('.entry.status-' + status).each(function(entry) {{
+                    if(entry.isVisible() != visible) {{
+                        entry.hide();
+                    }} else {{
+                        entry.show();
+                    }}
+                }})
+
+                if(!visible) {{
+                    toggle.set('text', 'Show ' + status + ' translations');
+                }} else {{
+                    toggle.set('text', 'Hide ' + status + ' translations');
+                }}
+
+                toggle.store('visible', visible);
+            }}
+
+            function after_entry_save(label, response, editor) {{
+                element = $(response.id);
+                element.removeClass('status-recent');
+                element.removeClass('status-stale');
+                element.removeClass('status-untranslated');
+                element.addClass('status-'+response.status);
+            }}
+
+            window.addEvent('domready', function() {{
+                $$('.translation').each(function(entry){{ new InlineEditor.Textarea(entry, {{url: 'save', method: 'JSONPOST', empty_msg: '<i>untranslated</i>', onSuccess: after_entry_save}}); }});
+            }})
+        </script>
+    </head>
+    <body>
+        <h1>Translate stringtable for {language}</h1>
+        <fieldset>
+        <legend>Display settings</legend>
+            <button type="button" class="toggle-recent" onclick="javascript:toggle_entries_by_status('recent')"><span>Hide recent translations</span></button>
+            <button type="button" class="toggle-stale" onclick="javascript:toggle_entries_by_status('stale')"><span>Hide stale translations</span></button>
+            <button type="button" class="toggle-untranslated" onclick="javascript:toggle_entries_by_status('untranslated')"><span>Hide untranslated translations</span></button>
+        </fieldset>
+        <table>
+            {entries}
+        </table>
+    </body>
+    <script>
+    </script>
+</html>
+    '''
+
+    entry = '''
+<tbody class="entry status-{status}" id="{key}">
+    <tr><th colspan="2">{key}</th></tr>
+    <tr class="notes"><td colspan="2">{notes}</td></tr>
+    <tr><td class="source">{source}</td><td class="translation" data-id="{key}">{translation}</td></tr>
+</tbody>
+    '''
+
+    def do_GET(self):
+        parsed_path = urlparse.urlparse(self.path)
+
+        if parsed_path.path == '/':
+            return self.GET_stringtable()
+        elif parsed_path.path == '/mootools-core.js':
+            return self.GET_url('https://cdnjs.cloudflare.com/ajax/libs/mootools/1.6.0/mootools-core.min.js')
+        elif parsed_path.path == '/mootools-more.js':
+            return self.GET_url('https://cdnjs.cloudflare.com/ajax/libs/mootools-more/1.6.0/mootools-more.js')
+        elif parsed_path.path == '/mootools-inlineeditor.js':
+            return self.GET_url('https://raw.githubusercontent.com/reednj/InlineEditor/455a4afbe65b4c5b445115929c073d1455d3b91d/Source/InlineEditor.js')
+        elif parsed_path.path == '/mootools-inlineeditor.css':
+            return self.GET_url('https://raw.githubusercontent.com/reednj/InlineEditor/455a4afbe65b4c5b445115929c073d1455d3b91d/Source/InlineEditor.css')
+        elif parsed_path.path == '/mootools-inlineeditor-textarea.js':
+            return self.GET_url('https://raw.githubusercontent.com/reednj/InlineEditor/455a4afbe65b4c5b445115929c073d1455d3b91d/Source/InlineEditor.Textarea.js')
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+    def GET_url(self, url):
+        response = urllib2.urlopen(url)
+        self.send_response(response.getcode())
+        self.send_header('Content-Type', response.info().gettype())
+        self.end_headers()
+
+        for chunk in response:
+            if '/mootools-inlineeditor-textarea.js' in url:
+                chunk = chunk.replace('this.edit_link.innerHTML = this.current_text;', '')
+            self.wfile.write(chunk)
+
+    def GET_stringtable(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html ; charset=utf-8')
+        self.end_headers()
+
+        entries = []
+
+        stat = StringTable.statistic(self.server.reference_st, self.server.source_st)
+
+        for key in self.server.reference_st.keys():
+            source = self.server.reference_st[key].value
+            source = cgi.escape(source)
+
+            translation = self.server.source_st[key].value if key in self.server.source_st else ''
+
+            status = 'recent'
+            if key not in stat.left_older:
+                status = 'stale'
+            if not translation:
+                status = 'untranslated'
+
+            translation = translation if translation else ''
+            translation = cgi.escape(translation)
+
+            notes = self.server.reference_st[key].notes
+            notes = '\n'.join(notes)
+            notes = cgi.escape(notes)
+
+            entries.append(EditServerHandler.entry.format(
+                key=key,
+                source=source,
+                translation=translation,
+                notes=notes,
+                status=status
+            ))
+
+        self.wfile.write(EditServerHandler.body.format(
+            source=self.server.source_st.fpath,
+            language=self.server.source_st.language,
+            entries=''.join(entries)
+        ))
+
+    def POST_error(self, message):
+        self.send_response(500)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+
+        self.wfile.write(json.dumps(
+            { 'code': 500, 'message': message }))
+
+    def do_POST(self):
+        try:
+            content_length = int(self.headers.get('Content-Length', '0'))
+            body = self.rfile.read(content_length)
+            request = json.loads(body)
+            if 'id' not in request or 'value' not in request:
+                raise ValueError("Some of the expected json keys are missing")
+        except ValueError:
+            self.POST_error("Browser didn't send a save request")
+            raise
+
+        if request['id'] not in self.server.reference_st:
+            self.POST_error("No entry with key {}".format(request['id']))
+
+        entry = self.server.source_st[request['id']]
+        rentry = self.server.reference_st[request['id']]
+
+        if entry.value != request['value']:
+            if request['value'] and rentry.value != request['value']:
+                entry.value = request['value']
+            else:
+                entry.value = None
+            if entry.value:
+                entry.value_times = [int(time.time())] * len(entry.value.split('\n'))
+            else:
+                entry.value_times = rentry.value_times
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+
+        status = 'recent'
+        if max(self.server.reference_st[request['id']].value_times) < min(entry.value_times):
+            status = 'stale'
+        if not entry.value:
+            status = 'untranslated'
+
+        response = {
+            'id': request['id'],
+            'status': status,
+        }
+
+        self.wfile.write(json.dumps(response))
+
 
 def format_action(args):
     source_st = StringTable.from_file(args.source)
@@ -322,31 +578,7 @@ def sync_action(args):
     reference_st = StringTable.from_file(args.reference)
     source_st = StringTable.from_file(args.source)
 
-    entries = []
-
-    for item in reference_st.items():
-        if isinstance(item, StringTableEntry):
-            if item.key in source_st:
-                source_item = source_st[item.key]
-                entries.append(StringTableEntry(
-                    item.key,
-                    item.keyline,
-                    source_item.value,
-                    item.notes,
-                    source_item.value_times
-                ))
-            else:
-                entries.append(StringTableEntry(
-                    item.key,
-                    item.keyline,
-                    None,
-                    item.notes,
-                    item.value_times
-                ))
-        else:
-            entries.append(item)
-
-    out_st = StringTable(source_st.fpath, source_st.language, source_st.notes, source_st.includes, entries)
+    out_st = StringTable.sync(reference_st, source_st)
 
     print(out_st, end='')
 
@@ -370,6 +602,19 @@ def rename_key_action(args):
             entry.key = args.new_key
 
     print(source_st, end='')
+
+
+def edit_action(args):
+    reference_st = StringTable.from_file(args.reference)
+    source_st = StringTable.from_file(args.source)
+
+    source_st = StringTable.sync(reference_st, source_st)
+
+    server = HTTPServer(('localhost', 8080), EditServerHandler)
+    server.reference_st = reference_st
+    server.source_st = source_st
+    webbrowser.open('http://localhost:8080/')
+    server.serve_forever()
 
 
 def check_action(args):
@@ -436,6 +681,11 @@ if __name__ == "__main__":
     rename_key_parser.add_argument('source', metavar='SOURCE', help="string table to rename old key within")
     rename_key_parser.add_argument('old_key', metavar='OLD_KEY', help="key to rename")
     rename_key_parser.add_argument('new_key', metavar='NEW_KEY', help="new key name")
+
+    edit_parser = verb_parsers.add_parser('edit', help="edit a stringtable and exit")
+    edit_parser.set_defaults(action=edit_action)
+    edit_parser.add_argument('reference', metavar='REFERENCE', help="reference string table")
+    edit_parser.add_argument('source', metavar='SOURCE', help="string table to edit")
 
     check_parser = verb_parsers.add_parser('check', help="check a stringtable for consistency and exit")
     check_parser.set_defaults(action=check_action)
